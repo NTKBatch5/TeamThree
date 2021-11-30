7a87e393ecd08ea17961a09bd4464ce7dd1d97ac
MSI <MSI@DESKTOP-KBKGS95>
2021-11-30 05:27:56 +0300

    added

--- src/three/TeamThree.java
+++ src/three/TeamThree.java
@@ -4,20 +4,25 @@ public class TeamThree {
 
 	public static void main(String[] args) {
 		System.out.println("Team Three Initial code...");
-		
+
 		System.out.println("Hello");
 	}
+
 	public static void shahid(String str) {
 		System.out.println("New method added");
 	}
+
 	public static void add(int a, int b) {
-		System.out.println(a+b);
+		System.out.println(a + b);
 		System.out.println("*************************************");
 	}
-	
+
 	public static void subtract(int a, int b) {
-		System.out.println(a-b);
+		System.out.println(a - b);
 	}
-	
+
+	public static void hayder(String str) {
+		System.out.println("New method added by hayder");
+	}
 
 }
