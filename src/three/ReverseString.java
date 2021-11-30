c997876254d5990f1ec017a35b3bd75d7dc2b205
ashahid91 <94854691+ashahid91@users.noreply.github.com>
2021-11-30 09:42:49 -0500

    added another way to reverse

--- src/three/ReverseString.java
+++ src/three/ReverseString.java
@@ -1,15 +1,32 @@
 package three;
+
 import java.util.*;
 
 public class ReverseString {
-	
+
 	public static void main(String[] args) {
-		
+
 		StringBuilder strBuild = new StringBuilder("NoorTeck");
-		
-		System.out.println("Reverse String is = " +	strBuild.reverse());
-		
-		
+
+		System.out.println("Reverse String is = " + strBuild.reverse());
+
+		// or we can apply this logic to reverse
+
+		String str = "GitHub";
+		// convert string to charArray using toCharArray() from string class
+
+		char[] strArray = str.toCharArray();
+		// will create new string to append
+		String nStr = "";
+		// now we loop from the last element of array and decrement by 1
+
+		for (int i = strArray.length - 1; i >= 0; i--) {
+
+			nStr = nStr + strArray[i];
+		}
+
+		System.out.println(nStr);
+
 	}
 
 }
