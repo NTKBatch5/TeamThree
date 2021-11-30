9c1bd9402b422a94d7e33183a55d3a22b06043e1
Owner <Owner@DESKTOP-GMJN2FU.myfiosgateway.com>
2021-11-30 09:33:50 -0500

    made new class and added reverse string object

---
+++ src/three/ReverseString.java
@@ -0,0 +1,15 @@
+package three;
+import java.util.*;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		StringBuilder strBuild = new StringBuilder("NoorTeck");
+		
+		System.out.println("Reverse String is = " +	strBuild.reverse());
+		
+		
+	}
+
+}
